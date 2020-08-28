ec8efcedd9ec6c71950072d9e725395f46ad46a6
Gabriel-Oest <gabriel.oest@xpo.com>
2020-08-27 23:17:35 -0300

    Add User repository do manage entity database integration

---
+++ src/main/java/com/nisum/interview/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nisum.interview.repository;
+
+import com.nisum.interview.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByEmail(String email);
+}
