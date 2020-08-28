3f562da232a50e21dcc5271ab9add90e41fbcdb2
Gabriel-Oest <gabriel.oest@xpo.com>
2020-08-28 00:38:12 -0300

    Add @Repository annotation

--- src/main/java/com/nisum/interview/repository/UserRepository.java
+++ src/main/java/com/nisum/interview/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.nisum.interview.repository;
 
 import com.nisum.interview.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     User findByEmail(String email);
