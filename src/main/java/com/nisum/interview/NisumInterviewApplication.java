bf87e58b43df2fb02f980ee5ebfeb3b813fac5d3
Gabriel-Oest <gabriel.oest@xpo.com>
2020-08-27 22:20:33 -0300

    first commit

---
+++ src/main/java/com/nisum/interview/NisumInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NisumInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nisum/interview/NisumInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
