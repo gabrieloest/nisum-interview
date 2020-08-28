e1284c206e15de9004999d19ab0ec818270e3b3a
Gabriel-Oest <gabriel.oest@xpo.com>
2020-08-28 16:26:54 -0300

    Update response codes

--- src/main/java/com/nisum/interview/advice/UserExceptionAdvice.java
+++ src/main/java/com/nisum/interview/advice/UserExceptionAdvice.java
@@ -1,12 +1,19 @@
 package com.nisum.interview.advice;
 
 import com.nisum.interview.dto.ResponseError;
+import com.nisum.interview.exception.EmailAlreadyInUseException;
+import com.nisum.interview.exception.InvalidEmailFormatException;
+import com.nisum.interview.exception.InvalidPasswordFormatException;
 import com.nisum.interview.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @ControllerAdvice
@@ -17,5 +24,34 @@ class UserExceptionAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     ResponseError userNotFoundHandler(UserNotFoundException ex) {
       return new ResponseError(ex.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(InvalidEmailFormatException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    ResponseError invalidEmailFormatHandler(InvalidEmailFormatException ex) {
+        return new ResponseError(ex.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(InvalidPasswordFormatException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    ResponseError invalidPasswordFormatHandler(InvalidPasswordFormatException ex) {
+        return new ResponseError(ex.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(EmailAlreadyInUseException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    ResponseError emailAlreadyInUseHandler(EmailAlreadyInUseException ex) {
+        return new ResponseError(ex.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    List<ResponseError> argumentNotValidHandler(MethodArgumentNotValidException ex) {
+        List<ResponseError> errors = ex.getBindingResult().getAllErrors().stream().map(item -> new ResponseError(item.getDefaultMessage())).collect(Collectors.toList());
+        return errors;
     }
 }
