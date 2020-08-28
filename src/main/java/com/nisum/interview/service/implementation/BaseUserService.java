16588def192af7bb8cbc14a92e8eea0bd909559e
Gabriel-Oest <gabriel.oest@xpo.com>
2020-08-28 16:27:50 -0300

    Add data validations

--- src/main/java/com/nisum/interview/service/implementation/BaseUserService.java
+++ src/main/java/com/nisum/interview/service/implementation/BaseUserService.java
@@ -1,18 +1,26 @@
 package com.nisum.interview.service.implementation;
 
+import com.nisum.interview.exception.InvalidEmailFormatException;
+import com.nisum.interview.exception.InvalidPasswordFormatException;
 import com.nisum.interview.exception.UserNotFoundException;
 import com.nisum.interview.model.User;
 import com.nisum.interview.repository.UserRepository;
 import com.nisum.interview.service.UserService;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Primary
 public class BaseUserService implements UserService {
+
+    private final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+    private final String PASSWORD_REGEX = "^(?=.*[0-9]{2,})(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).{4,}$";
 
     private final UserRepository userRepository;
 
@@ -25,6 +33,16 @@ public class BaseUserService implements UserService {
      */
     @Override
     public User save(User user) {
+        if(!isValidEmailFormat(user.getEmail())){
+            throw new InvalidEmailFormatException(user.getEmail());
+        }
+
+        if(!isValidPasswordFormat(user.getPassword())) {
+            throw new InvalidPasswordFormatException(user.getPassword());
+        }
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         return userRepository.save(user);
     }
 
@@ -59,5 +77,17 @@ public class BaseUserService implements UserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    private boolean isValidEmailFormat(String email){
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean isValidPasswordFormat(String password){
+        Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 }
