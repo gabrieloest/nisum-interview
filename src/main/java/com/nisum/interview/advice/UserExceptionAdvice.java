fdf251d606cf3a4c205c78517329a0b7e5f76cc7
Gabriel-Oest <gabriel.oest@xpo.com>
2020-08-28 09:36:06 -0300

    Add advice to treat User exceptions

--- src/main/java/com/nisum/interview/advice/UserExceptionAdvice.java
+++ src/main/java/com/nisum/interview/advice/UserExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.nisum.interview.advice;
 
+import com.nisum.interview.dto.ResponseError;
 import com.nisum.interview.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,13 +8,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ControllerAdvice
-class EmployeeNotFoundAdvice {
+class UserExceptionAdvice {
 
     @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String userNotFoundHandler(UserNotFoundException ex) {
-      return ex.getMessage();
+    ResponseError userNotFoundHandler(UserNotFoundException ex) {
+      return new ResponseError(ex.getMessage());
     }
 }
